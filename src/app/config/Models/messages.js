921cd2112e544de8c597f3d69420ffacc96023b3
Geeky <124719983+arooba-zaman1211@users.noreply.github.com>
2024-08-30 18:31:23 +0500

    admin panel

--- src/app/config/Models/messages.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose"
-
-const messagesModel = new mongoose.Schema({
-	contact_id: mongoose.Schema.Types.ObjectId,
-	description: String,
-},{ timestamps: true })
-
-export const Messages = mongoose.models.messages || mongoose.model('messages', messagesModel )
--- src/app/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose"
-
-export const connectionSrt = "mongodb+srv://user1211:aroobazaman@cluster0.zfmckuh.mongodb.net/softwareCompany?retryWrites=true&w=majority&appName=Cluster0"
-
-export const db = await mongoose.connect(connectionSrt)
